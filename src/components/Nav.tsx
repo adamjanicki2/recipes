c68dddf648f7c56ca41f088d8dcf9a62a36c845b
Adam Janicki <adamjanicki2@gmail.com>
2024-10-30 21:16:54 -0700

    version 1

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -28,7 +28,7 @@ const Nav = () => {
     <nav className="flex items-center justify-between w-100 nav pv2 ph4">
       <div className="flex items-center justify-between bar-container">
         <UnstyledLink className="nav-title" to="/">
-          Recipes
+          /recipes
         </UnstyledLink>
         <div className="mobile">
           <Hamburger open={open} onClick={() => setOpen(!open)} />
